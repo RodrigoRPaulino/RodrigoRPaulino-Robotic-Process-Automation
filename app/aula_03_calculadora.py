60e19a180013d6a00ef1d900d39480cf5126f1c3
Rodrigo Paulino <rodrigo.paulino@ibasolutions.com.br>
2026-04-06 09:06:58 -0300

    adicionando aula 03

---
+++ app/aula_03_calculadora.py
@@ -0,0 +1,18 @@
+import pyautogui as pg
+import time 
+
+#move por coordenada
+iniciar = pg.position(34,1056)
+pg.moveTo(iniciar)
+
+time.sleep(5)
+pg.click()
+
+time.sleep(2)
+calc = pg.typewrite("Calculadora")
+
+time.sleep(2)
+calc = pg.position(110,336)
+pg.click(calc)
+#serve para pegar a posição para pegar por coordenada
+#print(pg.position())
