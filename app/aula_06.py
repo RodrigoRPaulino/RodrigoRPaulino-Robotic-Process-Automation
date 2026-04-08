49249d68d319cac8f51443b404b4675505c6eaa0
Rodrigo Paulino <rodrigo.paulino@ibasolutions.com.br>
2026-04-08 00:27:56 -0300

    adicionando exercicio, modificando settings.json

---
+++ app/aula_06.py
@@ -0,0 +1,30 @@
+'''Exemplo de automação executando e populando um bloco de notas '''
+
+import pyautogui as pg
+import pygetwindow as getWindow
+import time as delay
+
+
+executar = pg.hotkey("win","r")
+delay.sleep(3)
+comando = pg.typewrite("Notepad")
+delay.sleep(3)
+pg.press("enter")
+
+delay.sleep(5)
+pg.typewrite("Escrevendo o texto via codigo")
+
+delay.sleep(3)
+
+#seleciona a jalena ativa para fechar 
+encerrar = getWindow.getActiveWindow()
+delay.sleep(3)
+
+encerrar.close() # type: ignore
+delay.sleep(3)
+
+print("Automação encerrada com sucesso!")
+
+
+
+
