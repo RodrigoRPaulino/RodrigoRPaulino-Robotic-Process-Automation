54282b11679a45addf6eee3469dd1c586262cab1
Rodrigo Paulino <rodrigo.paulino@ibasolutions.com.br>
2026-04-14 09:14:31 -0300

    implementando método para salvar arquivo xlsx

--- app/aula_08.py
+++ app/aula_08.py
@@ -1,23 +1,30 @@
-'''
-Intalação do selenium e biblioteca do pandas para extração de dados de tabela usando o openpyxl
-'''
-
-from selenium import webdriver   
+from selenium import webdriver
 from selenium.webdriver.common.by import By
+import pandas as pd
+
 driver = webdriver.Chrome()
+
+data_frame_lista: list[list[str]] = []
 
 driver.get("https://rpachallengeocr.azurewebsites.net/")
 
 # tabela
-tabela = driver.find_element(By.XPATH,'//*[@id="tableSandbox"]') 
-linhas =  tabela.find_elements(By.TAG_NAME,"tr") 
-coluna = tabela.find_elements(By.TAG_NAME,"td") 
+tabela = driver.find_element(By.XPATH, '//*[@id="tableSandbox"]')
+linhas = tabela.find_elements(By.TAG_NAME, "tr")
 
-linha = 1
-#para
 for linha_atual in linhas:
-    print(linha_atual.text)
-    linha = linha + 1
+    colunas = linha_atual.find_elements(By.TAG_NAME, "td")
+    valores = [coluna.text for coluna in colunas]
 
+    if valores:  # evita linha vazia (header)
+        print(valores)
+        data_frame_lista.append(valores)
 
+# cria dataframe
+df = pd.DataFrame(data_frame_lista)
 
+# salva direto
+df.to_excel("dadosSite.xlsx", index=False) # type: ignore
+
+input("Finalizado - pressione ENTER para fechar")
+driver.quit()
